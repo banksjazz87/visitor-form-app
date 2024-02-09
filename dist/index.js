169c481af18a4f985e8f1381696d7e24d9101d62
Chris <banksjazz87@gmail.com>
2024-02-08 21:00:22 -0500

    server: added some middleware

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 4900;
+//All middleware functions
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import express, { Express, Request, Response, Application} from 'express';
 import path from "path";
+import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 4900;
+
+//All middleware functions
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, ()=> {
     console.log(`App is listening on port ${port}`);
