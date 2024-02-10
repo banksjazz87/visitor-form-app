dab3a64d15521ff4ee97f004d10f275ffea61a1f
Chris <banksjazz87@gmail.com>
2024-02-09 22:26:04 -0500

    style: I have tailwind installed and configured

--- dist/client/src/App.jsx
+++ dist/client/src/App.jsx
@@ -4,18 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
-const logo_svg_1 = __importDefault(require("./logo.svg"));
-require("./App.css");
+require("../src/index.css");
 function App() {
-    return (<div className="App">
-      <header className="App-header">
-        <img src={logo_svg_1.default} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+    return (<div className="bg-slate-700">
+      <header>
+        <h1 className="text-3xl font-bold underline">Hello World</h1>
       </header>
     </div>);
 }
---
+++ dist/client/tailwind.config.js
@@ -0,0 +1,12 @@
+"use strict";
+// @type {import('tailwindcss').Config}
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./build/static/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
