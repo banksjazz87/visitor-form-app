2a07cf034654c6f83d4d5d1239a3d9fd23ea1f8d
Chris <banksjazz87@gmail.com>
2025-12-08 20:35:05 -0500

    feature: added pastor tim and chapel on the hill gmail to the email list

--- dist/Visitor-Form/index.js
+++ dist/Visitor-Form/index.js
@@ -235,7 +235,7 @@ app.post("/add-visitor-to-all", (req, res) => {
         return currentObj;
     });
     //Set up the email notification
-    const emailList = "banksjazz87@gmail.com, whitneylanematthews@yahoo.com, whitneymatthews05@gmail, chapelonthehill@embarqmail.com";
+    const emailList = "banksjazz87@gmail.com, whitneylanematthews@yahoo.com, whitneymatthews05@gmail, chapelonthehill@embarqmail.com, thefarki@icloud.com, chapelonthehillag316@gmail.com";
     const interestsString = interests.join(", ");
     const Email = new Mailer_1.Mailer(process.env.EMAIL_USER, process.env.EMAIL_PASSWORD, emailList, visitorData, interestsString);
     //Solo visitor
